26b6935f68ba52ef443e1b446f586eecd25db271
SrAndres6 <andresrjramillo671@gmail.com>
2025-09-10 17:01:07 -0500

    creacion del apartado de accesorios y algunas modificacciones

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,10 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBYVN1CcgOlpot5qrczGBMmhVkk5hIGHH8",
+  authDomain: "dllo-ejercicio.firebaseapp.com",
+  projectId: "dllo-ejercicio",
+  storageBucket: "dllo-ejercicio.firebasestorage.app",
+  messagingSenderId: "673481486761",
+  appId: "1:673481486761:web:1fb4d778b08fe6196314b8",
+  measurementId: "G-8DTH1L5ERJ"
 };
 
 // Previene inicialización doble en Next.js
@@ -14,3 +17,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
